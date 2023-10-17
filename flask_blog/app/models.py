14808abfb35f70cbaa1dcce26764a4ece5a65eb7
Andrey Kuzmin <andromed-ku@yandex.ru>
2023-10-17 15:19:46 +0500

    Updates to the `User` model to support user logins

--- flask_blog/app/models.py
+++ flask_blog/app/models.py
@@ -1,5 +1,8 @@
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from . import db
-from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -11,9 +14,10 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
@@ -28,6 +32,8 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
